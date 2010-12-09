55979b989ce452899244267f9626cccb91df1a3d
Alexander Lockshyn <info@lab37.com>
2010-12-10 00:46:07 +0200

    Added kohana framework

---
+++ application/config/config.php
@@ -0,0 +1,9 @@
+<?php
+$config['modules'] = array
+(
+	MODPATH.'auth',      // Authentication
+);
+
+$config['log_threshold'] = 4;
+$config['log_directory'] = APPPATH.'logs';
+?>
---
+++ application/logs/2010/12/10.php
@@ -0,0 +1,4 @@
+<?php defined('SYSPATH') or die('No direct script access.'); ?>
+
+2010-12-10 00:43:00 --- ERROR: ErrorException [ 4 ]: syntax error, unexpected T_VARIABLE ~ APPPATH/classes/controller/welcome.php [ 28 ]
+2010-12-10 00:43:18 --- ERROR: ErrorException [ 4 ]: syntax error, unexpected T_VARIABLE ~ APPPATH/classes/controller/welcome.php [ 28 ]
